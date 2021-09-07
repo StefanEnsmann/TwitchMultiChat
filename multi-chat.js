1218bee0006fbbbc256f4aa1a25a295fd1e6c3e2
Stefan Ensmann <stefan@ensmann.de>
2021-09-08 01:30:45 +0200

    started updating with jquery

--- multi-chat.js
+++ multi-chat.js
@@ -1,57 +1,75 @@
-var content = document.body;
-var template =
-    "<div class=\"box\" style=\"float:left;width:$w$%;height:$h$%\">" + 
-        "<div class=\"row header\" style=\"padding-left:10px;padding-top:5px;padding-bottom:5px\"><a href=\"https://www.twitch.tv/$channel$\" target=\"_blank\">$channelDisplay$</a></div>" + 
-        "<div class=\"row content\">" + 
-            "<iframe id=\"$id$\" src=\"https://www.twitch.tv/embed/$channel$/chat?parent=tools.ensmann.de$darkmode$\" style=\"border:none;width:100%;height:100%\">iframes not supported</iframe>" + 
-        "</div>" + 
-    "</div>";
-
-var urlParams = parseURLParams(window.location.href);
-console.log(urlParams);
+let userNameRegEx = /^[a-zA-Z0-9_]{4,25}$/;
+let users = [];
+let darkMode = false;
+let rowCount = 0;
 
 function init() {
-    if (urlParams.get("query").get("c") === undefined) {
-        alert("You have not provided any channels to view. Returning to configurator...");
-        window.location.replace("https://z.ensmann.de/multi-chat/");
-    }
-    else {
-        startChats()
+    let urlParams = parseURLParams(window.location.href);
+    console.log(urlParams);
+    let q = urlParams.get("query");
+
+    darkMode = q.get("dm") !== undefined && q.get("dm").length > 0 ? true : false;
+    $("#multiChatDarkMode").checked = darkMode;
+    darkModeChanged(darkMode);
+
+    rowCount = q.get("rc") !== undefined && !isNaN(parseInt(q.get("rc"))) ? parseInt(q.get("rc")) : 1;
+    $("#rowCount").value = rowCount;
+    rowCountChanged(rowCount);
+    
+    paramUsers = q.get("c");
+
+    if (paramUsers !== undefined) {
+        users = paramUsers;
+        $("#multiChatChannels").value = ";".join(users);
+        loadChats()
     }
 }
 
-function startChats() {
-    var q = urlParams.get("query");
-    var channels = q.get("c");
-    var useDarkMode = q.get("dm") !== undefined && q.get("dm").length > 0 ? "&darkpopout" : "";
-    var useTwoRows = q.get("tr") !== undefined && q.get("tr").length > 0;
-
-    if (useDarkMode !== "") {
-        document.body.style.backgroundColor = "#000";
+function getUserList() {
+    let userString = $("multiChatChannels").value;
+    let users = userString.split(";")
+    let finalUsers = [];
+    let wrongUsers = [];
+    users.forEach(user => {if (user === "") return; if (userNameRegEx.test(user)) finalUsers.push(user); else wrongUsers.push(user);});
+    if (wrongUsers.length > 0) {
+        alert("The following users are no valid Twitch account names: " + wrongUsers.join(", "));
     }
+    return finalUsers;
+}
 
-    console.log("starting chats");
-    console.log(channels);
-    console.log(useDarkMode);
-    console.log(useTwoRows);
+function generateURL() {
+    let url = "https://tools.ensmann.de/multi-chat/";
+    let hasQM = false;
+    if (users.length > 0) {
+        url += "?c=" + "&c=".join(users);
+        hasQM = true;
+    }
+    if (darkMode) {
+        url += hasQM ? "&" : "?";
+        hasQM = true;
+        url += "dm";
+    }
+    if (rowCount > 1) {
+        url += hasQM ? "&" : "?";
+        hasQM = true;
+        url += "rc=" + rowCount;
+    }
+    return generateURL;
+}
 
-    var title = "Multi-Chat: " + channels.join(", ");
-    window.document.title = title;
-    
-    var newContent = "";
+function loadChats() {
+    users = getUserList();
+    window.document.title = "Multi-Chat: " + users.join(", ");
+    $(".chat.box").remove();
+}
 
-    var chanCount = Math.min(10, channels.length);
-    var height = (useTwoRows && channels.length > 1) ? "50" : "100";
-    var dividor = (useTwoRows && channels.length > 1) ? Math.floor(chanCount / 2) : chanCount;
-    var width1 = 100 / dividor;
-    var width2 = 100 / (chanCount - dividor);
-
-    var index = 0;
-    for (var i = 0; i < 2; ++i) {
-        var limit = i == 0 ? Math.min(dividor, chanCount) : chanCount;
+function startChats() {
+    let index = 0;
+    for (let i = 0; i < 2; ++i) {
+        let limit = i == 0 ? Math.min(dividor, chanCount) : chanCount;
         for (; index < limit; ++index) {
             channels[index] = channels[index].trim()
-            var currentFrame = template
+            let currentFrame = template
             .replace("$id$", "chat" + index)
             .replace("$channelDisplay$", channels[index].trim())
             .replace(/\$channel\$/g, channels[index].trim().toLowerCase())
@@ -64,7 +82,106 @@ function startChats() {
             newContent = newContent + "<br />";
         }
     }
-    content.innerHTML = newContent;
+}
+
+function darkModeChanged(useDarkMode) {
+    console.log("darkModeChanged triggered");
+    darkMode = useDarkMode;
+    $("body").css("background-color", darkMode ? "black": "");
+}
+
+function rowCountChanged(rowValue) {
+    console.log("rowCountChanged triggered");
+    rowCount = rowValue;
+    let chatRows = $(".chatrow");
+    if (chatRows.length < rowCount) {
+        let chatbox = $("#chatbox");
+        for (let index=chatRows.length; index < rowCount; ++index) {
+            let div = document.createElement("div");
+            div.classList.add("chatrow", "row", "content");
+            chatbox.append(div);
+        }
+    }
+    distributeChatsToRows(chatRows);
+    if (chatRows.length > rowCount) {
+        chatRows.each(function(index) {
+            if (index >= rowCount) {
+                $(this).remove();
+            }
+        });
+    }
+}
+
+function distributeChatsToRows(chatRows) {
+    let chatsPerRow = users.length / rowCount;
+    let chats = $(".chat.box");
+    chats.detach();
+    chatRows.each(function(index) {
+        if (index == rowCount - 1) {
+            $(this).append(chats.slice(index * chatsPerRow));
+        }
+        else {
+            $(this).append(chats.slice(index * chatsPerRow, (index + 1) * chatsPerRow - 1));
+        }
+    });
+}
+
+function openMultiStreams() {
+    if (users.length > 0) {
+        let url = null;
+        let service = document.getElementById("multiChatStreams").value;
+        switch (service) {
+                case "ms":
+                if (users.length > 8) {
+                    while (users.length > 8) {
+                        users.pop();
+                    }
+                    alert("Since MultiStre.am supports a maximum amount of 8 streams, your request has been shortened to the 8 first channels!");
+                }
+                url = "https://multistre.am/" + users.join("/");
+                break;
+            case "mt":
+                url = "http://multitwitch.tv/" + users.join("/");
+                break;
+            case "mtb":
+                url = "https://multitwitch.live/" + users.join("/");
+                break;
+            case "tt":
+                url = "https://twitchtheater.tv/" + users.join("/");
+                break;
+            default:
+                alert("There was an error opening the streaming service!");
+        }
+        if (url != null) {
+            window.open(url);
+        }
+    }
+}
+
+function generateChatBox(width, height, channel, id, darkmode) {
+    let lowerChannel = channel.toLowerCase();
+    let outerDiv = document.createElement("div");
+    outerDiv.classList.add("chat", "box");
+    outerDiv.style.float = "left";
+    outerDiv.style.width = width + "%";
+    outerDiv.style.height = height + "%";
+    let header = document.createElement("div");
+    header.classList.add("row", "chat", "header");
+    let channelLink = document.createElement("a");
+    channelLink.href = "https://www.twitch.tv/" + lowerChannel;
+    channelLink.target = "_blank";
+    header.appendChild(channelLink);
+    outerDiv.appendChild(header);
+    let chatBoxDiv = document.createElement("div");
+    chatBoxDiv.classList.add("row", "content");
+    let iFrame = document.createElement("iframe");
+    iFrame.classList.add("chatframe");
+    iFrame.id = "chat" + id;
+    iFrame.src = "https://www.twitch.tv/embed/" + lowerChannel + "/chat?parent=tools.ensmann.de$darkmode$" + (darkmode ? "&darkpopout" : "");
+    iFrame.innerHTML = "iframes not supported";
+    chatBoxDiv.appendChild(iFrame);
+    outerDiv.appendChild(chatBoxDiv);
+    return outerDiv;
 }
 
 window.onload = init;
